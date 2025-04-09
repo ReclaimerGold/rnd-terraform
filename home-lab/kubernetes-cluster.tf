831a59eaca4b5187a50dd78ca4ac70635d794199
Ryan T. M. Reiffenberger <ryan@ftghost.net>
2025-04-09 20:03:44 +0000

    feat: Added Kubernetes Cluster & Updated Config
    Added Kubernetes cluster components, and updated the config file to include the username (due to issues with !(bang) not being supported in the 'export' command. Added TLS Insecure option to bypass self-signed certificate issues set by default in the PVE.
    
    Removes requirement to use 'export' command to set PM_API_TOKEN_ID environment variable when executing Terraform commands.

--- home-lab/kubernetes-cluster.tf
+++ home-lab/kubernetes-cluster.tf
@@ -1,3 +1,6 @@
+### Virtual Machines
+
+# Control Plane 01
 resource "proxmox_vm_qemu" "talos-m01" {
     name            = "talos-m01"
     target_node     = "vm-01"
@@ -7,32 +10,257 @@ resource "proxmox_vm_qemu" "talos-m01" {
     # sockets         = 1
     cores           = 4
     tags            = "kubernetes,kcp"
-    iso             = "nas-isos:iso/talos-metal-amd64.iso"
 
-    disk { # Root Disk
-        type        = "scsi"
-        slot        = 0
-        size        = "20G"
-        storage     = "nas-vdisk"
-    }
-
-    disk { # Storage Disk
-        type        = "scsi"
-        slot        = 1
-        size        = "100G"
-        storage     = "nas-vdisk"
-    }
-
-    disk { # Storage Disk
-        type        = "cdrom"
-        slot        = 2
-        iso         = "nas-isos:iso/talos-metal-amd64.iso"
-        storage = "nas-isos"
+    disks {
+      scsi {
+        scsi0 {
+          disk { # Root Disk
+            size        = "20G"
+            storage     = "nas-vdisk"
+          }
+        }
+        scsi1 {
+          disk { # Storage Disk
+            size        = "100G"
+            storage     = "nas-vdisk"
+          }
+        }
+      }
+      ide {
+        ide2 {
+          cdrom {
+            iso = "nas-isos:iso/talos-metal-amd64.iso"
+          }
+        }
+      }
     }
 
     network { # Primary Interface
+        id          = 0
         model       = "virtio"
         bridge      = "vmbr0"
         firewall    = true
     }
 }
+
+# Control Plane 02
+resource "proxmox_vm_qemu" "talos-m02" {
+    name            = "talos-m02"
+    target_node     = "vm-01" # Set to 'vm-01' while 'vm-02' is in hibernation
+    agent           = 1
+    memory          = 4096
+    balloon         = 2048
+    # sockets         = 1
+    cores           = 4
+    tags            = "kubernetes,kcp"
+
+    disks {
+      scsi {
+        scsi0 {
+          disk { # Root Disk
+            size        = "20G"
+            storage     = "nas-vdisk"
+          }
+        }
+        scsi1 {
+          disk { # Storage Disk
+            size        = "100G"
+            storage     = "nas-vdisk"
+          }
+        }
+      }
+      ide {
+        ide2 {
+          cdrom {
+            iso = "nas-isos:iso/talos-metal-amd64.iso"
+          }
+        }
+      }
+    }
+
+    network { # Primary Interface
+        id          = 0
+        model       = "virtio"
+        bridge      = "vmbr0"
+        firewall    = true
+    }
+}
+
+# Control Plane 03
+resource "proxmox_vm_qemu" "talos-m03" {
+    name            = "talos-m03"
+    target_node     = "vm-03"
+    agent           = 1
+    memory          = 4096
+    balloon         = 2048
+    # sockets         = 1
+    cores           = 4
+    tags            = "kubernetes,kcp"
+
+    disks {
+      scsi {
+        scsi0 {
+          disk { # Root Disk
+            size        = "20G"
+            storage     = "nas-vdisk"
+          }
+        }
+        scsi1 {
+          disk { # Storage Disk
+            size        = "100G"
+            storage     = "nas-vdisk"
+          }
+        }
+      }
+      ide {
+        ide2 {
+          cdrom {
+            iso = "nas-isos:iso/talos-metal-amd64.iso"
+          }
+        }
+      }
+    }
+
+    network { # Primary Interface
+        id          = 0
+        model       = "virtio"
+        bridge      = "vmbr0"
+        firewall    = true
+    }
+}
+
+# Worker 01
+resource "proxmox_vm_qemu" "talos-w01" {
+    name            = "talos-w01"
+    target_node     = "vm-01"
+    agent           = 1
+    memory          = 2048
+    balloon         = 2048
+    # sockets         = 1
+    cores           = 2
+    tags            = "kubernetes,kw"
+
+    disks {
+      scsi {
+        scsi0 {
+          disk { # Root Disk
+            size        = "20G"
+            storage     = "nas-vdisk"
+          }
+        }
+        scsi1 {
+          disk { # Storage Disk
+            size        = "100G"
+            storage     = "nas-vdisk"
+          }
+        }
+      }
+      ide {
+        ide2 {
+          cdrom {
+            iso = "nas-isos:iso/talos-metal-amd64.iso"
+          }
+        }
+      }
+    }
+
+    network { # Primary Interface
+        id          = 0
+        model       = "virtio"
+        bridge      = "vmbr0"
+        firewall    = true
+    }
+}
+
+# Worker 02
+resource "proxmox_vm_qemu" "talos-w02" {
+    name            = "talos-w02"
+    target_node     = "vm-01" # Set to 'vm-01' while 'vm-02' is in hibernation
+    agent           = 1
+    memory          = 2048
+    balloon         = 2048
+    # sockets         = 1
+    cores           = 2
+    tags            = "kubernetes,kw"
+
+    disks {
+      scsi {
+        scsi0 {
+          disk { # Root Disk
+            size        = "20G"
+            storage     = "nas-vdisk"
+          }
+        }
+        scsi1 {
+          disk { # Storage Disk
+            size        = "100G"
+            storage     = "nas-vdisk"
+          }
+        }
+      }
+      ide {
+        ide2 {
+          cdrom {
+            iso = "nas-isos:iso/talos-metal-amd64.iso"
+          }
+        }
+      }
+    }
+
+    network { # Primary Interface
+        id          = 0
+        model       = "virtio"
+        bridge      = "vmbr0"
+        firewall    = true
+    }
+}
+
+# Worker 03
+resource "proxmox_vm_qemu" "talos-w03" {
+    name            = "talos-w03"
+    target_node     = "vm-03"
+    agent           = 1
+    memory          = 2048
+    balloon         = 2048
+    # sockets         = 1
+    cores           = 2
+    tags            = "kubernetes,kw"
+
+    disks {
+      scsi {
+        scsi0 {
+          disk { # Root Disk
+            size        = "20G"
+            storage     = "nas-vdisk"
+          }
+        }
+        scsi1 {
+          disk { # Storage Disk
+            size        = "100G"
+            storage     = "nas-vdisk"
+          }
+        }
+      }
+      ide {
+        ide2 {
+          cdrom {
+            iso = "nas-isos:iso/talos-metal-amd64.iso"
+          }
+        }
+      }
+    }
+
+    network { # Primary Interface
+        id          = 0
+        model       = "virtio"
+        bridge      = "vmbr0"
+        firewall    = true
+    }
+}
+
+### LXC Containers
+# TODO: Add in container registration for cluster proxy system. Ideally want to use HA Proxy to allow for future expansion of proxy services in cluster mode.
+
+### HA Proxy Configuration
+# TODO: Utlilize provider listed below to create HA Proxy configuration for above LXC Declaration for HA Proxy
+# https://registry.terraform.io/providers/SepehrImanian/haproxy/latest/docs
