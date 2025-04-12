50a1ff6967cbf2cf324cbfabe1a32767793ba318
Ryan T. M. Reiffenberger <ryan@ftghost.net>
2025-04-12 00:31:17 +0000

    build: Added talos provider to Terraform

--- home-lab/config.tf
+++ home-lab/config.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "telmate/proxmox"
       version = "3.0.1-rc8" # or whatever version you're using
     }
+    talos = {
+      source  = "siderolabs/talos"
+      version = "0.7.1"
+    }
   }
 }
 
@@ -13,3 +17,5 @@ provider "proxmox" {
   pm_api_token_secret = var.pm_api_token_secret
   pm_tls_insecure     = var.pm_tls_insecure
 }
+
+provider "talos" {}
